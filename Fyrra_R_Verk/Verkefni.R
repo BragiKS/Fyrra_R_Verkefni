241d1686887c5039110a0e12b4eb77177c9a481e
BragiKS <bks9@hi.is>
2024-02-16 14:49:06 +0000

    a), b), c) og d) lidir done

--- Fyrra_R_Verk/Verkefni.R
+++ Fyrra_R_Verk/Verkefni.R
@@ -3,5 +3,14 @@
 
 install.packages("tidyverse")
 
+# a)
 bsss <- read_csv2("https://ahj.hi.is/kaupskra.csv", locale = locale(encoding = "ISO8859-1"))
 
+# b)
+bsss <- filter(bsss, tegund == "Serbyli" | tegund == "Fjolbyli")
+
+# c)
+bsss <- bsss %>% mutate(fermetraverd = kaupverd / einflm)
+
+# d)
+bsss <- bsss %>% mutate(ar = year(thinglystdags))
